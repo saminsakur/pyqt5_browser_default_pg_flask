edd87e3debf5a65c468855e400a738f7abcddfc7
samin <saminsakur06@gmail.com>
2021-10-12 11:14:58 +0600

    Initial commit ✨

--- app.py
+++ app.py
@@ -10,4 +10,8 @@ app = Flask(__name__)
 def server():
     return render_template("index.html")
 
-# app.run(debug=True)
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Page not found!", 404
+
+app.run(debug=True, host="0.0.0.0")
